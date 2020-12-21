7f0cdb79c7d1cab6a0f79fd7100683a90fee342f
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-12-21 16:06:41 +0300

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -1,15 +1,11 @@
 import java.util.Scanner;
 import java.util.TreeSet;
 import java.util.regex.Pattern;
-
 public class Address {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         TreeSet<String> mailSet = new TreeSet<>();
-
         String[] s;
-
         while (true) {
             s = sc.nextLine().split("\\s+");
             switch (s[0]) {
@@ -19,20 +15,15 @@ public class Address {
                     else
                         System.out.println("Неверный ввод");
                     break;
-
                 case ("LIST"):
                     for (String mail : mailSet)
                         System.out.println(mail);
-
                     break;
-
                 default:
                     System.out.println("Данная команда отсутствует");
             }
         }
     }
-
-
     static boolean isMailCorrect(String s) {
         return Pattern.matches("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$", s);
     }
